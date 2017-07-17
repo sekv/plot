9dae8943e49b990c7fafbdb73407b7fb89629d81
rainycheng <rainycheng1988@163.com>
2017-07-17 19:55:43 +1000

    add auto parameter

--- Likelihood_distribution.py
+++ Likelihood_distribution.py
@@ -7,53 +7,74 @@ matplotlib.use('Agg')
 import numpy as np
 import matplotlib.ticker as ticker
 import matplotlib.pyplot as plt
+import sys
 
-XNUM = 50
-data = np.loadtxt('5_10_20_train_result.txt')
-data1 = np.loadtxt('5_10_20_test_result.txt')
-vmin = np.ndarray.min(data)
-vmax = np.ndarray.max(data)
+cluster_num = [5,10,15,20,25]
+component_num = [10,20,30]
+window_num = [20, 50, 100,200]
+dir = 'c2pro/'
+dirpic = 'c2pro_distlog/'
 
-dist=[1]*XNUM
-dist1=[1]*XNUM
-for i in range(0,XNUM):
-    dist[i]=0
-    dist1[i]=0
-print dist
+for a in cluster_num:
+	for b in component_num:
+		for c in window_num:
 
-seg = [1]*XNUM
-for i in range(0,XNUM):
-    seg[i]= vmin + (vmax-vmin)/XNUM*i
-print seg
-print seg[0]
+			XNUM = 60
+			data = np.loadtxt(dir+str(a)+'_'+str(b)+'_'+str(c)+'_'+'train_result.txt')
+			data1 = np.loadtxt(dir+str(a)+'_'+str(b)+'_'+str(c)+'_'+'test_result.txt')
+			vmin1 = np.ndarray.min(data)
+			vmin2 = np.ndarray.min(data1)
+			vmax1 = np.ndarray.max(data)
+			vmax2 = np.ndarray.max(data1)
+			if(vmin1 <= vmin2):
+				vmin = vmin1
+			else:
+				vmin = vmin2
+			if(vmax1 > vmax2):
+				vmax = vmax1 
+			else:
+				vmax = vmax2
 
-#count the number of data items fall in segment seg[j]
-for i in range(0,data.size):
-    for j in range(0,XNUM):
-        if(data[i]>=seg[XNUM-1-j]):
-          dist[XNUM-1-j] = dist[XNUM-1-j] + 1
-          break
+			dist=[1]*XNUM
+			dist1=[1]*XNUM
+			for i in range(0,XNUM):
+			    dist[i]=0
+			    dist1[i]=0
+			#print dist
 
-for i in range(0,data1.size):
-    for j in range(0,XNUM):
-        if(data1[i]>=seg[XNUM-1-j]):
-          dist1[XNUM-1-j] = dist1[XNUM-1-j] + 1
-          break
+			seg = [1]*XNUM
+			for i in range(0,XNUM):
+			    seg[i]= vmin + (vmax-vmin)/XNUM*i
+			#print seg
+			#print seg[0]
 
-distPtrain = [1]*XNUM
-distPtest = [1]*XNUM
-for i in range(0,XNUM):
-    distPtrain[i] = dist[i]*1.0/data.size
-    distPtest[i] = dist1[i]*1.0/data1.size
+			#count the number of data items fall in segment seg[j]
+			for i in range(0,data.size):
+			    for j in range(0,XNUM):
+			        if(data[i]>=seg[XNUM-1-j]):
+			          dist[XNUM-1-j] = dist[XNUM-1-j] + 1
+			          break
 
-FSIZE = 25
-plt.figure(figsize=(10,8),frameon=True)
-matplotlib.rc('xtick', labelsize=FSIZE)
-matplotlib.rc('ytick',labelsize=FSIZE)
-plt.plot(seg,distPtrain,'ro--',label='train',lw=7,markersize=15)
-plt.plot(seg,distPtest,'ys-',label='test',lw=5,markersize=15)
-plt.xlabel('Log Likelihood',fontsize=FSIZE)
-plt.ylabel('Probability',fontsize=FSIZE)
-plt.legend(loc='upper left',numpoints=1,fontsize=FSIZE)
-plt.show()
-plt.savefig("LikelihoodDistribution.png")
+			for i in range(0,data1.size):
+			    for j in range(0,XNUM):
+			        if(data1[i]>=seg[XNUM-1-j]):
+			          dist1[XNUM-1-j] = dist1[XNUM-1-j] + 1
+			          break
+
+			distPtrain = [1]*XNUM
+			distPtest = [1]*XNUM
+			for i in range(0,XNUM):
+			    distPtrain[i] = dist[i]*1.0/data.size
+			    distPtest[i] = dist1[i]*1.0/data1.size
+
+			FSIZE = 25
+			plt.figure(figsize=(10,8),frameon=True)
+			matplotlib.rc('xtick', labelsize=FSIZE)
+			matplotlib.rc('ytick',labelsize=FSIZE)
+			plt.plot(seg,distPtrain,'ro--',label='train',lw=7,markersize=15)
+			plt.plot(seg,distPtest,'ys-',label='test',lw=5,markersize=15)
+			plt.xlabel('Log Likelihood',fontsize=FSIZE)
+			plt.ylabel('Probability',fontsize=FSIZE)
+			plt.legend(loc='upper left',numpoints=1,fontsize=FSIZE)
+			plt.savefig(dirpic+str(a)+'_'+str(b)+'_'+str(c)+"logdist.png")
+			plt.close()
